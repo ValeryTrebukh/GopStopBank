e9c09f74a7246e614fca53a4a895b09e31383795
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-10-19 01:41:45 +0300

    model layer

---
+++ src/main/java/com/elesson/gopstopbank/repository/Dao.java
@@ -0,0 +1,15 @@
+package com.elesson.gopstopbank.repository;
+
+import com.elesson.gopstopbank.model.AbstractEntity;
+import java.util.List;
+
+public interface Dao {
+
+    AbstractEntity save(AbstractEntity entity);
+
+    AbstractEntity get(Integer id);
+
+    List<AbstractEntity> getAll();
+
+    boolean delete(Integer id);
+}

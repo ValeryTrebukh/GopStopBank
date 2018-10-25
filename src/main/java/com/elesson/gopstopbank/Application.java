314ee91dbbecceb576e43594f3df1b2e6cefaf47
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-10-25 08:22:48 +0300

    changed springboot version

--- src/main/java/com/elesson/gopstopbank/Application.java
+++ src/main/java/com/elesson/gopstopbank/Application.java
@@ -2,8 +2,10 @@ package com.elesson.gopstopbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication (scanBasePackages={"com.elesson.gopstopbank"})
+@SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
 
     public static void main(String[] args) {
